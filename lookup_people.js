aaf6584169f8c441bba84b98b25f2a20d0b7e1bf
Felipe Pardo Suarez <felipe.pardosuarez@gmail.com>
2017-02-01 01:52:14 +0000

    Refactored lookup_people

--- lookup_people.js
+++ lookup_people.js
@@ -1,6 +1,41 @@
 const pg = require("pg");
 const settings = require("./settings"); // settings.json
 
+function printThatWeAreSearching() {
+  console.log("Searching ...");
+}
+
+function printResults(queryName, result) {
+  let length = result.rows.length;
+  console.log(`Found ${length} person(s) by the name ${queryName}:`);
+
+  var printPerson = (row) => {
+    let id = row.id;
+    let first_name = row.first_name;
+    let last_name = row.last_name;
+    let birthdate = row.birthdate;
+    console.log(`- ${id}: ${first_name} ${last_name}, born ${birthdate}`);
+  }
+  result.rows.forEach(printPerson);
+}
+
+function queryForFamousPeople(client, queryArgument, preQueryAction, postQueryAction) {
+  client.connect((err) => {
+    if (err) {
+      return console.error("Connection Error", err);
+    }
+    preQueryAction();
+    const query = "SELECT * FROM famous_people WHERE last_name = $1::text";
+    client.query(query, [queryArgument], (err, result) => {
+      if (err) {
+        return console.error("error running query", err);
+      }
+      postQueryAction(result)
+      client.end();
+    });
+
+  });
+}
 
 const input = process.argv[2];
 
@@ -13,27 +48,4 @@ const client = new pg.Client({
   ssl      : settings.ssl
 });
 
-client.connect((err) => {
-  if (err) {
-    return console.error("Connection Error", err);
-  }
-  client.query("SELECT * FROM famous_people WHERE last_name = $1::text", [input], (err, result) => {
-    if (err) {
-      return console.error("error running query", err);
-    }
-    result.rows.forEach((item) => {
-      let id = item.id;
-      let first_name = item.first_name;
-      let last_name = item.last_name;
-      let birthdate = item.birthdate;
-      let length = result.rows.length;
-
-      console.log("Searching ...");
-      console.log(`Found ${length} person(s) by the name ${input}:`);
-      console.log(`- ${id}: ${first_name} ${last_name}, born ${birthdate}`);
-    });
-
-    client.end();
-  });
-});
-
+queryForFamousPeople(client, input, printThatWeAreSearching, printResults.bind(null, input));
